18a3d3591d9c8f8faec41b13bdd592afaec2d7ad
Sergey Tsvetkov <tsvetkov71@gmail.com>
2022-05-17 13:18:18 +0300

    Add Lab1Ex4(divideIt)

---
+++ Lab1/Exercise4/DivideIt.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    internal class DivideIt
+    {
+        static void Main(string[] args)
+        {
+            try 
+            {
+                Console.WriteLine("Enter first number");
+                string temp = Console.ReadLine();
+                int i = Int32.Parse(temp);
+                Console.WriteLine("Enter second number");
+                temp = Console.ReadLine();
+                int j = Int32.Parse(temp);
+                int k = i / j;
+                Console.WriteLine(k);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An exception was thrown: {0}", ex);
+            }
+            
+
+        }
+    }
+}
