3ef403ef15f5d8da6c8ecaf3618b0295d60f1bc1
Sergey Tsvetkov <tsvetkov71@gmail.com>
2022-05-23 11:46:22 +0300

    Add.Lab4Ex1

---
+++ Lab4/Ex.1Utility/Utils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex._1Utility
+{
+    internal class Utils
+    {
+        public static int Greater(int a, int b)
+        {
+            if (a > b)
+                return a;
+            else
+                return b;
+        }
+    }
+}
