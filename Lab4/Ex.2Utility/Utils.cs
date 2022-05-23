1a145e00b1a6e4e54c0ae08dbb5ea6b1b438cd34
Sergey Tsvetkov <tsvetkov71@gmail.com>
2022-05-23 12:02:59 +0300

    Add.Lab4Ex2

---
+++ Lab4/Ex.2Utility/Utils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex._2Utility
+{
+    internal class Utils
+    {
+        public static int Greater(int a, int b)
+        {
+            if (a > b)
+                return a;
+            else
+                return b;
+        }
+        public static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
